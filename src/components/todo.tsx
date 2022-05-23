208c886346b7079a468041665d48f92d554f49b3
Leo A <>
2022-05-23 23:09:07 +0900

    aaa

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,3 +1,22 @@
+import React, { useState } from "react";
+import { TodoForm } from "./todo_form";
+import { TodoList } from "./todo_list";
+
 export const Todo = () => {
-  return <div>todo</div>;
-};
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+  return todos.map((todo, index) => {
+    <div
+      className={todo.isComplete ? "todo_row complete" : "todo_row"}
+      key={index}
+    >
+
+      <div key={todo.id} onClick{()=> completeTodo(todo.id)}>
+      {todo.text}
+      </div>
+
+    </div>
+  })
+}
