9d6a66eed29242fe142c7811fa804617c1c26f63
Leo A <>
2022-05-24 01:30:00 +0900

    use map to list the Todo list under the form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import "./App.css";
 
 export const App = () => {
@@ -14,11 +14,24 @@ export const App = () => {
     setInputValue(e.target.value);
   };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    };
+
+    setTodos([newTodo, ...todos]);
+    setInputValue("");
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Todo list</h2>
-        <form onSubmit={() => {}}>
+        <form onSubmit={(e) => handleSubmit(e)}>
           <input
             type="text"
             onChange={(e) => handleChange(e)}
@@ -26,6 +39,11 @@ export const App = () => {
           />
           <input type="submit" value="Add" className="submitButton" />
         </form>
+        <ul className="todoList">
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.inputValue}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
