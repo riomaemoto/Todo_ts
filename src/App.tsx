a89062e2c416be73f45062de4a87f83bc58759ad
Leo A <>
2022-05-24 01:14:20 +0900

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import { ChangeEvent, useState } from "react";
 import "./App.css";
 
 export const App = () => {
-  return <div className="App">aaaa</div>;
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Todo list</h2>
+        <form onSubmit={() => {}}>
+          <input
+            type="text"
+            onChange={(e) => handleChange(e)}
+            className="inputText"
+          />
+          <input type="submit" value="Add" className="submitButton" />
+        </form>
+      </div>
+    </div>
+  );
 };
